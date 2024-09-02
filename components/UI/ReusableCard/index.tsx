b6d8fb3c7bc9cad0eb4b20ca07732e5355a86301
Alisher-00kg <alisher.estebesov@gmail.com>
2024-09-02 03:30:28 +0300

    create cardModalWindow and add any styles

--- components/UI/ReusableCard/index.tsx
+++ components/UI/ReusableCard/index.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import { Card, Image, Text, Badge, Group } from "@mantine/core";
 import classes from "./style.module.css";
-import { Button } from "../../../theme/components";
+import { Button as ThemeButton } from "../../../theme/components";
+import ModalWindow from "../CardModalWindow";
 
 type ProductCardProps = {
   image: string;
@@ -17,37 +19,60 @@ const ProductCard: React.FC<ProductCardProps> = ({
   weight,
   price,
 }) => {
+  const [opened, setOpened] = useState(false);
+
+  const handleOpen = () => setOpened(true);
+  const handleClose = () => setOpened(false);
+
   return (
-    <Card
-      shadow="sm"
-      padding="lg"
-      radius="md"
-      withBorder
-      className={classes.cardContainer}
-    >
-      <Card.Section>
-        <Image
-          src={image || "https://shorturl.at/gKTtg"}
-          alt={name}
-          className={classes.img}
-        />
-      </Card.Section>
+    <>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+        className={classes.cardContainer}
+        onClick={handleOpen}
+      >
+        <Card.Section>
+          <Image
+            src={image || "https://shorturl.at/gKTtg"}
+            alt={name}
+            className={classes.img}
+          />
+        </Card.Section>
 
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{name}</Text>
-        <Badge color="pink">{weight}</Badge>
-      </Group>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500} className={classes.headingTitle}>
+            {name}
+          </Text>
+          <Badge color="pink">{weight}</Badge>
+        </Group>
 
-      <Text size="sm" color="dimmed">
-        {composition}
-      </Text>
+        <Text size="md" color="#5A6A70" className={classes.mainText}>
+          {composition}
+        </Text>
 
-      <Text size="lg" fw={700} mt="md">
-        {price} руб.
-      </Text>
-
-      <Button variant="outline">Добавить в корзину</Button>
-    </Card>
+        <div className={classes.priceAndButton}>
+          <Text size="lg" fw={700} mt="md">
+            {price} руб.
+          </Text>
+          <ThemeButton variant="outline" className={classes.toBasketButton}>
+            Добавить в корзину
+          </ThemeButton>
+        </div>
+      </Card>
+      <ModalWindow
+        opened={opened}
+        onClose={handleClose}
+        title={name}
+        image={image || "https://shorturl.at/gKTtg"}
+        description={composition}
+        price={price}
+        size="780px"
+        weight={weight}
+      />
+    </>
   );
 };
 
